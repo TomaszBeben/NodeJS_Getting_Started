8bde2f902338e3ea1b944af69fadbab59e3c3ac7
TomaszBeben <tbeben@st.swps.edu.pl>
2022-03-24 19:02:08 +0100

    4-modules/5-errors

--- 4-modules/5-errors/1-loop.js
+++ 4-modules/5-errors/1-loop.js
@@ -1,10 +1,9 @@
-const path = require('path');
+// const path = require('path');
 const fs = require('fs');
 
-const files = ['.bash_profile', '.npmrc'];
+const files = ['./1-loop.js','./zzzz', './2-try.js'];
 
 files.forEach(file => {
-  const filePath = path.resolve(process.env.HOME, file);
-  const data = fs.readFileSync(filePath);
+  const data = fs.readFileSync(file);
   console.log('File data is', data);
 });
--- 4-modules/5-errors/3-throw.js
+++ 4-modules/5-errors/3-throw.js
@@ -1,12 +1,10 @@
-const path = require('path');
 const fs = require('fs');
 
-const files = ['.bash_profile', 'kjkjhh', '.npmrc'];
+const files = ['./1-loop.js','./zzzz', './2-try.js'];
 
 files.forEach(file => {
   try {
-    const filePath = path.resolve(process.env.HOME, file);
-    const data = fs.readFileSync(filePath);
+    const data = fs.readFileSync(file);
     console.log('File data is', data);
   } catch (err) {
     if (err.code === 'ENOENT') {
